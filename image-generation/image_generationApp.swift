5a706666641322d74364113396ef8b6d47c78b1c
“Nathan <schroedernathan2011@icloud.com>
2025-03-27 17:43:26 -0400

    Initial Commit

---
+++ image-generation/image_generationApp.swift
@@ -0,0 +1,17 @@
+//
+//  image_generationApp.swift
+//  image-generation
+//
+//  Created by Nathan Schroeder on 2025-03-27.
+//
+
+import SwiftUI
+
+@main
+struct image_generationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

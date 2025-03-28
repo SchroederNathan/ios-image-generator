777dae18579b0722c454b7aa65d97a4943cc1685
“Nathan <schroedernathan2011@icloud.com>
2025-03-27 23:58:23 -0400

    Created theme and basic home page

--- image-generation/ContentView.swift
+++ image-generation/ContentView.swift
@@ -7,15 +7,50 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Group{
+                Text("Images go here")
+                
+                VStack(alignment: .leading, spacing: 12) {
+                    Text("Generate Images \nin Your Pocket")
+                        .font(Font.custom("Poppins-Medium", size: 34))
+                        .lineLimit(nil)
+                        .foregroundColor(Color("Text"))
+                        .fixedSize(horizontal: false, vertical: true)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    Text("This is some random text on what this app really does. Something about generating em")
+                        .font(Font.custom("Poppins-Regular", size: 20))
+                        .foregroundColor(Color("Text"))
+                        .lineLimit(nil)
+                        .fixedSize(horizontal: false, vertical: true)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }
+                
+                .padding()
+                .padding(.top, 60.0)
+                
+                Button(action: {
+                    print("tapped!")
+                }, label: {
+                    Text("Get Started")
+                        .font(Font.custom("Poppins-Regular", size: 20))
+                        .fontWeight(.medium)
+                        .foregroundColor(Color("Background"))
+                        .frame(maxWidth: .infinity, minHeight: 60)
+                        .background(Color("Primary"))
+                        .cornerRadius(16)
+                        .padding()
+                })
+            }.frame(maxHeight: .infinity, alignment: .bottom)
+                
         }
-        .padding()
+        .background(Color("Background"))
     }
 }
 
